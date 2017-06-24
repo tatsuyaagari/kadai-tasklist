d01172f11db167b0dcc94ea57d1682e67b1c743d
tatsuyaagari <kanri.1217@gmail.com>
2017-06-24 08:44:23 +0000

    bootstrap design

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 10 }
-  validates :status, presence: true, length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 30 }
+  validates :status, presence: true, length: { maximum: 30 }
 end
