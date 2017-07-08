356c529d415fbfda4d4c192ec1fb59c7bb2c9cb3
tatsuyaagari <kanri.1217@gmail.com>
2017-07-08 10:25:52 +0000

    before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -56,4 +57,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+
 end
